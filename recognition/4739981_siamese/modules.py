96459afd891579e59cf7c70f3bcd6aa97cce4311
Alex-Mewis <xelavmew@gmail.com>
2025-10-12 12:44:09 +1000

    add simple binary classifier as a network which will be used to classify the Siamese networks latent vectors.

--- recognition/4739981_siamese/modules.py
+++ recognition/4739981_siamese/modules.py
@@ -20,4 +20,28 @@ class SiameseNetwork(nn.Module):
         """
         y1 = self._backbone(x1)
         y2 = self._backbone(x2)
-        return y1, y2  
+        return y1, y2 
+
+
+class Classifier(nn.Module):
+
+    def __init__(self, input_dim: int = 1000) -> None:
+        super(Classifier, self).__init__()
+
+        self._fcl = nn.Sequential(
+            nn.Linear(input_dim, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 2),
+            nn.SoftMax(inplace=True),
+        )
+
+        return None
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self._fcl(x)
