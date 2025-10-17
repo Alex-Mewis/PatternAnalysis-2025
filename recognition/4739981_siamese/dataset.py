6c9b3fa7cf464daf52010e1c3a9304b506684ecf
Alex-Mewis <xelavmew@gmail.com>
2025-10-17 10:56:32 +1000

    changed custom dataset to load in the images when being indexed into and just store the filepaths in memeory. This way we can load in the whole dataset without running out of memory.

--- recognition/4739981_siamese/dataset.py
+++ recognition/4739981_siamese/dataset.py
@@ -3,7 +3,7 @@ Contains the data loader for loading and preprocesing the data.
 """
 import os, time, random, threading
 from typing import Self
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor 
 import pandas as pd
 from PIL import Image
 import numpy as np
@@ -13,7 +13,9 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
 PERCENTAGE_OF_DATA_TO_LOAD = 0.1 
-THREADS_USE = 4 
+THREADS_USE = 4
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 to_tesnor_transform = transforms.ToTensor()
 
@@ -37,74 +39,92 @@ def progress_bar_animation(progress: float, num_boxes:int = 66, completed_symbol
     
     return None
 
-def launch_progress_bar(n, total: int):
-    
+def launch_progress_bar(n, total: int) -> None:
+
     def update_progress_bar() -> None:
-        print("Started Loading in Data")
+        print("Started Loading in images")
         start_time = time.time()
         while True:
             progress = n[0]/total
             progress_bar_animation(progress)
-            if progress == 1:
-                break
+            if progress == 1: break
             time.sleep(0.1)
-
+        
         elapsed_time = time.time() - start_time
-        print(f"\nLoading in Images took {elapsed_time:.3f}s or {(elapsed_time/60):3f}mins")
+        print(f"\nLoading in Images took {elapsed_time:.3f}s or {(elapsed_time/60):.3f}mins")
         return None
-    
+
     thread = threading.Thread(target=update_progress_bar)
     thread.daemon = True
     thread.start()
-    return
 
-def load_image_data(args: list) -> None:
-    image_path, image_tensors, i, n = args
-    image = Image.open(image_path)
-    image_tensors[i] =  to_tesnor_transform(image)
+    return None 
+
+def fetch_image_data(filepath: str) -> None:
+    image = Image.open(filepath)
+    return to_tesnor_transform(image).to(device)
+
+def load_image(args: list) -> None: 
+    i, image_filepath, image_filepaths, labels_df, labels, n = args
+    
+    image_filepaths[i] = image_filepath 
+
+    image_name = os.path.basename(image_filepath).replace('.jpg', '')        
+    label = labels_df.loc[labels_df['image_name'] == image_name, 'target'].iloc[0]
+    labels[i] = label
+
     n[0] += 1
+
+    return None
+
+def get_data_stats(labels_path: str) -> None:
+    """
+    """
+    df = pd.read_csv(labels_path)
+    n = len(df)
+    n_benign = (df['target'] == 0).sum()
+    n_malignant = (df['target'] == 1).sum()
+
+    print(f"Number of datapoints = {n}")
+    print(f"Number of benign     = {n_benign} => {(100*n_benign/n):.3f}%")
+    print(f"Number of maligant   = {n_malignant}   => {(100*n_malignant/n):.3f}%")
+
     return None
 
 class ISICImageDataset(Dataset):
     """
     """
     def __init__(self, image_dir: str, labels_path: str,
-                 shortcut_images: torch.Tensor | None = None,
+                 shortcut_images: np.ndarray | None = None,
                  shortcut_labels: np.ndarray | None = None) -> None:
 
         if shortcut_images is not None and shortcut_labels is not None:
             assert len(shortcut_labels) == len(shortcut_images)
             self._len = len(shortcut_labels)
-            self._images_data = shortcut_images
+            self._image_filepaths = shortcut_images
             self._labels = shortcut_labels
             return None
 
         # get the image data.
         self._len = int(len(os.listdir(image_dir)) * PERCENTAGE_OF_DATA_TO_LOAD)
-        image_tensors = [None] * self._len 
-        image_names = [None] * self._len
-        image_args = [None] * self._len
-        number_of_images_loaded = [0] 
-        for i, image_name in enumerate(os.listdir(image_dir)):
-            if i >= self._len: break
-            image_names[i] = image_name.replace('.jpg', '')
-            image_args[i] = [os.path.join(image_dir, image_name), image_tensors, i, number_of_images_loaded]
-
-        launch_progress_bar(number_of_images_loaded, self._len)
-
-        with ThreadPoolExecutor(max_workers=THREADS_USE) as thread_excecuter:
-            for args in image_args:
-                thread_excecuter.submit(load_image_data, args)
         
-        self._images_data = torch.stack(image_tensors, dim=0)
-
-        # get the labels data.
         labels_df = pd.read_csv(labels_path)
         self._labels = np.zeros(self._len)
-        for i, image_name in enumerate(image_names):
-            label = labels_df.loc[labels_df['image_name'] == image_name, 'target'].iloc[0]
-            self._labels[i] = label
+        loading_args = [None] * self._len
+        self._image_filepaths = np.empty(self._len, dtype=object)
+        number_of_images_loaded = [0]
+        
+        for i, image_name in enumerate(os.listdir(image_dir)):
+            if i >= self._len: break
+            loading_args[i] = [i, os.path.join(image_dir, image_name), self._image_filepaths, 
+                               labels_df, self._labels, number_of_images_loaded]
 
+        launch_progress_bar(number_of_images_loaded, self._len)
+        
+        with ThreadPoolExecutor(max_workers=THREADS_USE) as thread_executer:
+            for args in loading_args:
+                thread_executer.submit(load_image, args)
+        
         self._iter_pairwise = True
         
         return None
@@ -116,7 +136,7 @@ class ISICImageDataset(Dataset):
         if (i < 0 or i >= self._len):
             raise IndexError
         
-        image = self._images_data[i]
+        image = fetch_image_data(self._image_filepaths[i])
         label = self._labels[i]
 
         if not self._iter_pairwise:
@@ -131,7 +151,7 @@ class ISICImageDataset(Dataset):
             pair_label = self._labels[pair_i]
             found_pair = pair_label == label
 
-        pair_image = self._images_data[pair_i]
+        pair_image = fetch_image_data(self._image_filepaths[pair_i])
         return image, pair_image, label
     
     def set_iter_pairwise(self, iter_pairwise: bool) -> None:
@@ -144,7 +164,7 @@ class ISICImageDataset(Dataset):
         shuffled_indicies = list(range(self._len)) 
         random.shuffle(shuffled_indicies)
         
-        self._images_data = self._images_data[shuffled_indicies]
+        self._image_filepaths = self._image_filepaths[shuffled_indicies]
         self._labels = self._labels[shuffled_indicies]
         
         return None
@@ -156,16 +176,21 @@ class ISICImageDataset(Dataset):
 
         train_n = int(p*self._len)
 
-        train_images = self._images_data[:train_n]
-        test_images = self._images_data[train_n:]
+        train_image_filepaths = self._image_filepaths[:train_n]
+        test_image_filepaths = self._image_filepaths[train_n:]
         
         train_labels = self._labels[:train_n]
         test_labels = self._labels[train_n:]
         
-        train_dataset = ISICImageDataset(None, None, train_images, train_labels)
-        test_dataset = ISICImageDataset(None, None, test_images, test_labels)
+        train_dataset = ISICImageDataset(None, None, train_image_filepaths, train_labels)
+        test_dataset = ISICImageDataset(None, None, test_image_filepaths, test_labels)
 
         return train_dataset, test_dataset
 
     def to_DataLoader(self, **kwargs) -> DataLoader:
         return DataLoader(self, **kwargs)
+    
+
+
+if __name__ == "__main__":
+    get_data_stats('./data/ISIC_2020_Training_GroundTruth.csv')
